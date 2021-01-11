5f099047ba11384daa2e123c945501dd4eb9866c
Itai Mapaya <mambostudioz@gmail.com>
2021-01-11 12:58:01 -0600

    Add desktop media queries

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const headerEl = document.querySelector("header");
 toggleMobileNav.addEventListener("click", () => {
   if (toggleMobileNav.classList.contains("icon-hamburger")) {
     // change icon
-    toggleMobileNav.innerHTML = `
+    toggleMobileNav.innerHTML = /* html */ `
       <path d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z" fill="#FFF" fill-rule="evenodd"/>
   `;
     toggleMobileNav.setAttribute("width", "20");
@@ -21,7 +21,7 @@ toggleMobileNav.addEventListener("click", () => {
     document.body.classList.add("scroll");
   } else {
     // change icon
-    toggleMobileNav.innerHTML = `
+    toggleMobileNav.innerHTML = /* html */ `
       <g fill="#FFF" fill-rule="evenodd"><path d="M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z"/></g>
       `;
     toggleMobileNav.setAttribute("width", "24");
